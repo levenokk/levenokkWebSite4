a87231d34e23321f0d452386c7519c189a8c6566
levenokk <levenokk@yandex.ru>
2020-02-09 20:31:20 +0300

    optimization

--- app/js/main.js
+++ app/js/main.js
@@ -1,19 +1,28 @@
 $(function () {
 
+    AOS.init();
 
     $('.map').on('click', function (event) {
         if (event.target.closest('.map-marker')) {
 
-            $('.description-show').addClass('active');
+            $('.description-show').addClass('active').animate({ 'opacity': 1 }, 200);
             $('.description-show').find('h5').html(event.target.closest('.map-marker').querySelector('.market__info p:nth-child(1)').innerHTML.trim());
             $('.description-show').find('.description__text p').html(event.target.closest('.map-marker').querySelector('.market__info p:nth-child(2)').innerHTML.trim());
             $('.description-show').find('.description__suptext p').html(event.target.closest('.map-marker').querySelector('.market__info p:nth-child(3)').innerHTML.trim());
 
+            let destination = $('.description-show').offset().top;
+
+            $('body, html').animate({ scrollTop: destination }, 500); //1100 - скорость
+
         } else {
 
-            $('.description-show').removeClass('active');
+            $('.description-show').animate({ 'opacity': 0 }, 200, function () {
+                $('.description-show').removeClass('active');
+            });
 
         }
-    })
+    });
+
+    $(window).lazyLoadXT();
 
 });
