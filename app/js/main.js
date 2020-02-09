eaf4a968b0671f76e3dafc4ddca25679a82c4526
levenokk <levenokk@yandex.ru>
2020-02-09 14:58:29 +0300

    Закончен сайт

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,19 @@
-$(function(){
+$(function () {
 
 
+    $('.map').on('click', function (event) {
+        if (event.target.closest('.map-marker')) {
 
-  
+            $('.description-show').addClass('active');
+            $('.description-show').find('h5').html(event.target.closest('.map-marker').querySelector('.market__info p:nth-child(1)').innerHTML.trim());
+            $('.description-show').find('.description__text p').html(event.target.closest('.map-marker').querySelector('.market__info p:nth-child(2)').innerHTML.trim());
+            $('.description-show').find('.description__suptext p').html(event.target.closest('.map-marker').querySelector('.market__info p:nth-child(3)').innerHTML.trim());
+
+        } else {
+
+            $('.description-show').removeClass('active');
+
+        }
+    })
+
 });
